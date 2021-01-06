d46dca36183fd65ce58d9bbc37a47904132606d5
Robb Tran <bobtran@usc.edu>
2021-01-06 00:55:03 -0800

    Video Cars and Pedestrians Detection

--- Car_and_Pedestrian_Tracking.py
+++ Car_and_Pedestrian_Tracking.py
@@ -1,36 +1,42 @@
 import cv2
 from random import randrange
 
-#Image source
-street = 'street.jpg'
-
+#Video source
+video = cv2.VideoCapture('dashcam.mp4')
 #Create car and pedestrian classifier from trained data
 car_tracker = cv2.CascadeClassifier('cars.xml')
 people_tracker = cv2.CascadeClassifier('haarcascade_fullbody.xml')
 
-#Create opencv Image
-street_img = cv2.imread(street)
+while True:
+    #Read each frame
+    (success, frame) = video.read()
 
-#Convert to black and white
-street_img_gs = cv2.cvtColor(street_img, cv2.COLOR_BGR2GRAY)
+    #If can't read skip to next frame
+    if success==False:
+        continue
 
-#Detect the car coords and people coords
-car_coords = car_tracker.detectMultiScale(street_img_gs)
-people_coords = people_tracker.detectMultiScale(street_img_gs)
-print(car_coords)
-print(people_coords)
+    #Convert to grayscale
+    frame_gs = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-#Draw rectangles over each car and pedestrian
-for (x, y, w, h) in car_coords:
-    cv2.rectangle(street_img, (x, y), (x+w, y+h), (randrange(256), randrange(256), randrange(256)), 2)
-for (x, y, w, h) in people_coords:
-    cv2.rectangle(street_img, (x, y), (x+w, y+h), (randrange(256), randrange(256), randrange(256)), 2)
+    #Get the coords of cars and pedestrians
+    car_coords = car_tracker.detectMultiScale(frame_gs)
+    people_coords = people_tracker.detectMultiScale(frame_gs)
 
+    #Draw rectangle shapes over them
+    for (x, y, w, h) in car_coords:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 256), 2)
+    for (x, y, w, h) in people_coords:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 256, 0), 2)
 
-#Display image with rectangles drawn over car and people
-cv2.imshow('Car and Pedestrian Image', street_img)
+    #Render
+    cv2.imshow('Life Footage', frame)
+    key = cv2.waitKey(1)
 
-#Necessary for display
-cv2.waitKey()
+    #End if q or Q is pressed
+    if key == 81 or key == 113:
+        break
+
+#Release
+video.release()
 
 print("Code Completed")
